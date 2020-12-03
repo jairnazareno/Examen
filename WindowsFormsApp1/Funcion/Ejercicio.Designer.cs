07970124c7e4f8c5e800aee8643db2f05b586ada
JAIR NAZARENO <jairnazareno12@gmail.com>
2020-12-03 12:51:58 -0500

    exa

---
+++ WindowsFormsApp1/Funcion/Ejercicio.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace WindowsFormsApp1.Funcion
+{
+    partial class Ejercicio
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRespuesta = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(47, 67);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EL RESULTADO FINAL ES  ";
+            // 
+            // txtRespuesta
+            // 
+            this.txtRespuesta.Location = new System.Drawing.Point(273, 67);
+            this.txtRespuesta.Name = "txtRespuesta";
+            this.txtRespuesta.Size = new System.Drawing.Size(138, 20);
+            this.txtRespuesta.TabIndex = 1;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(51, 115);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 2;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Ejercicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(605, 209);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtRespuesta);
+            this.Controls.Add(this.label1);
+            this.Name = "Ejercicio";
+            this.Text = "Ejercicio";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRespuesta;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
---
+++ WindowsFormsApp1/Metodos/Metodo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metodos
+{
+    class Metodo
+    {
+        public static string procesofactorial(int b)
+        {
+            int j;
+            double re = 1;
+            string a;
+            for (j = 1; j <= b; j++)
+            {
+                re = re * j;
+            }
+            a = re + Environment.NewLine;
+            return a;
+        }
+    }
+}
